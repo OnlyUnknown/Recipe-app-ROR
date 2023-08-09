9d1c16fe2a5e790057d6478cca0aab907daa0a72
Abdullah <abdullahkhrief@gmail.com>
2023-08-09 19:14:43 +0300

    have added the ability to add the recipe by the specified user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.where(user_id: current_user.id).includes(:user)
   end
 
   # GET /recipes/1 or /recipes/1.json
@@ -12,6 +12,7 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+    @recipe.user_id = current_user.id
   end
 
   # GET /recipes/1/edit
