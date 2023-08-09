b9a493b63951822c838f75ef007f64c531769346
Abdullah <abdullahkhrief@gmail.com>
2023-08-09 17:13:30 +0300

    fixed N+1 queries

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,7 @@ class FoodsController < ApplicationController
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.where(user_id: current_user.id)
+    @foods = Food.where(user_id: current_user.id).includes(:user)
   end
 
   # GET /foods/1 or /foods/1.json
