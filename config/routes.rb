ef518b75dcc9c171328312ee2b7407ff3d601b4a
James Serengia <jamesserengia@gmail.com>
2023-08-10 09:10:32 +0300

    Add and configured devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipe_foods
   resources :foods
   resources :recipes
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "recipes#index"
 end
